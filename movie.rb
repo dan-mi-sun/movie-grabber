44ef5a5fd9174b90cbfde99f39a94a31398b3f62
Daniel Sun <dan.mi.sun@gmail.com>
2014-04-29 19:19:21 +0100

    add parameter name to template

--- movie.rb
+++ movie.rb
@@ -1,16 +1,26 @@
 class Movie
 
   # Add attr_accessors for the values you want to store...
+  attr_accessor :title
+  attr_accessor :actors
+  attr_accessor :director
+  attr_accessor :year
+  attr_accessor :plot
 
   def self.get_film_info(name)
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
-    binding.pry
 
     # Create a Movie object...
     m = Movie.new
 
     # Fill in the attributes...
+    m.title = movie_info["Title"] 
+    m.actors = movie_info["Actors"]
+    m.director = movie_info["Director"]
+    m.year = movie_info["Year"]
+    m.plot = movie_info["Plot"]
+    return m
 
     # Store me in a database...
   end
--- movie_app.rb
+++ movie_app.rb
@@ -6,16 +6,18 @@ require_relative 'movie'
 
 get '/' do
   # Just have a nice welcome page
-  erb :home
+  erb :movie, { :locals => {
+  :m => params["name"]
+  } }
 end
 
 post '/film' do
   # Search for a Movie
   # HINT - what is in params ?
-  binding.pry
+ # "You picked #{params["name"]}"
 
   # Lookup the film information on the web
-  film = Movie.get_film_info("jaws")
+  film = Movie.get_film_info(params["name"])
 
   # store the film in the database
 
